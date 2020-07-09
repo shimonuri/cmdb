bb31edd190e76f85b15448cc5c20c21885abf635
shimonuri <54213090+shimonuri@users.noreply.github.com>
2020-07-09 23:30:22 +0300

    Improving cli doc

--- efocus/__main__.py
+++ efocus/__main__.py
@@ -4,17 +4,27 @@ import sys
 import pathlib
 
 
-@click.command()
-@click.argument("script", default=None)
-@click.option("--function", default=".*", help="The function name to filter.")
-def main(script, function):
-    if not script:
-        return
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
+
+@click.argument("script")
+@click.option(
+    "-f",
+    "--function",
+    default=".*",
+    help="Show only output originated by function that matches regex.",
+)
+@click.command(name="python", context_settings=CONTEXT_SETTINGS)
+def main(script, function):
+    """
+    Used to filter prints and to log desired execution data.
+
+    SCRIPT is the path to the python script to be executed.
+    """
     preload.implement(function)
     sys.path.insert(0, str(pathlib.Path(script).parent))
     exec(open(script, "rt").read(), globals())
 
 
 if __name__ == "__main__":
-    main()
+    main(prog_name="python3 -m efocus")  # Call with explicit `prog_name`
